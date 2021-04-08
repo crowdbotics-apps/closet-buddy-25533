48a57ba0e8f22ae5079686242c47e3102dd82d6f
Crowdbotics <team@crowdbotics.com>
2021-04-08 03:24:34 +0000

    Update models

--- backend/closet_buddy_25533/wsgi.py
+++ backend/closet_buddy_25533/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'closet_buddy_25533.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "closet_buddy_25533.settings")
 
 application = get_wsgi_application()
